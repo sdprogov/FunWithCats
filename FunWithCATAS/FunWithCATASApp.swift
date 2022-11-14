cfdda4569edfd16d5395138fa90169fcadbf2389
Stefan Progovac <Stefan.progovac@catapultsports.com>
2022-11-14 15:15:25 -0500

    Initial Commit

---
+++ FunWithCATAS/FunWithCATASApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunWithCATASApp.swift
+//  FunWithCATAS
+//
+//  Created by Stefan Progovac on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct FunWithCATASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
